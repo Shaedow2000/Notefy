525fcd5405e0c82fa1e9c00efeacd188b6d12064
Shaedow2000 <amsal8795@gmail.com>
2026-01-04 14:42:46 +0100

    Create main function & Create main NotesApp class.

---
+++ functions.py
@@ -0,0 +1,23 @@
+import json
+
+class NotesApp:
+    def __init__( self, dir: str ) -> None:
+        self.dir: str = dir
+
+    def rewrite_json( self ) -> None:
+        pass
+
+    def add_note( self ) -> None:
+        pass
+
+    def remove_note( self ) -> None:
+        pass
+
+    def show_all_notes( self ) -> None:
+        pass
+
+    def read_note( self ) -> None:
+        pass
+
+    def update_note( self ) -> None:
+        pass
---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+from gui import ui
+
+def main() -> None:
+    pass
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit( 1 )
