07b482087b492203cc7c274c09d56fb96e571782
Shaedow2000 <amsal8795@gmail.com>
2026-01-16 09:09:40 +0100

    Create menu buttons.

--- ui.py
+++ ui.py
@@ -12,14 +12,23 @@ read_menu: Frame = Frame( window )
 show_all_menu: Frame = Frame( window )
 
 def ui() -> None:
+    # GLOBAL VARS
+    fonts: tuple = ( 'Impact', 16 )
+
     window.geometry( '1000x700' )
     window.minsize( 1000, 700 )
 
     top_label: Label = Label( window, text='Notefy', font=( 'Impact', 26, 'bold' ), fg='black', bg='skyblue', relief=FLAT, bd=14 )
     top_label.pack( fill='x' )
 
-    button_side: Frame = Frame( window )
+    buttons_side: Frame = Frame( window )
 
-    button_side.pack( side='left', fill='y' )
+    add_button: Button = Button( buttons_side, text='Add Note', font=fonts, width=25, command=lambda: ( add_menu.pack() ) )
+    remove_button = Button = Button( buttons_side, text='Remove Note', font=fonts, width=25, command=lambda: ( remove_menu.pack() ) )
+    update_button: Button= Button( buttons_side, text='Update Note', font=fonts, width=25, command=lambda: ( update_menu.pack() ) )
+    read_button: Button = Button( buttons_side, text='Read Note', font=fonts, width=25, command=lambda: ( read_menu.pack() ) )
+    show_all_button: Button = Button( buttons_side, text='Show All Note', font=fonts, width=25, command=lambda: ( show_all_menu.pack() ) )
+
+    buttons_side.pack( side='left', fill='y' )
 
     window.mainloop()
