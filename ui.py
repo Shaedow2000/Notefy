fd4de227f10f5ad51ec1f428a9a792aa353bd865
Shaedow2000 <amsal8795@gmail.com>
2026-01-16 08:48:52 +0100

    Declare the new window and frames.

--- ui.py
+++ ui.py
@@ -5,5 +5,13 @@ notesapp: NotesApp = NotesApp()
 
 notesapp.create_json_file()
 
+window: Tk = Tk()
+
+add_menu: Frame = Frame( window )
+remove_menu: Frame = Frame( window )
+update_menu: Frame = Frame( window )
+read_menu: Frame = Frame( window )
+show_all_menu: Frame = Frame( window )
+
 def ui() -> None:
-    pass
+    pass 
