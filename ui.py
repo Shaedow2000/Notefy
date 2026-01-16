9ff949cea9d733cfacc84d11c224278f2d5744a0
Shaedow2000 <amsal8795@gmail.com>
2026-01-16 09:11:27 +0100

    Fix syntax error & Pack all buttons.

--- ui.py
+++ ui.py
@@ -24,10 +24,16 @@ def ui() -> None:
     buttons_side: Frame = Frame( window )
 
     add_button: Button = Button( buttons_side, text='Add Note', font=fonts, width=25, command=lambda: ( add_menu.pack() ) )
-    remove_button = Button = Button( buttons_side, text='Remove Note', font=fonts, width=25, command=lambda: ( remove_menu.pack() ) )
-    update_button: Button= Button( buttons_side, text='Update Note', font=fonts, width=25, command=lambda: ( update_menu.pack() ) )
+    remove_button: Button = Button( buttons_side, text='Remove Note', font=fonts, width=25, command=lambda: ( remove_menu.pack() ) )
+    update_button: Button = Button( buttons_side, text='Update Note', font=fonts, width=25, command=lambda: ( update_menu.pack() ) )
     read_button: Button = Button( buttons_side, text='Read Note', font=fonts, width=25, command=lambda: ( read_menu.pack() ) )
     show_all_button: Button = Button( buttons_side, text='Show All Note', font=fonts, width=25, command=lambda: ( show_all_menu.pack() ) )
+
+    add_button.pack( pady=5 )
+    remove_button.pack( pady=5 )
+    update_button.pack( pady=5 )
+    read_button.pack( pady=5 )
+    show_all_button.pack( pady=5 )
 
     buttons_side.pack( side='left', fill='y' )
 
